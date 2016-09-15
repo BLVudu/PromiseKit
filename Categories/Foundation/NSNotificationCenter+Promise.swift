20d7f5263e33686b31a0231f8d819000b1085310
plin2 <ping-hsien.lin@vuud.com>
2016-09-15 13:56:10 -0700

    Fixed Xcode 8 Archive Swift Error Reference: https://gist.github.com/mnespor/be0d60bfefc04e54608b3cf67cbf0c43

--- Categories/Foundation/NSNotificationCenter+Promise.swift
+++ Categories/Foundation/NSNotificationCenter+Promise.swift
@@ -18,39 +18,40 @@ import PromiseKit
 
     import PromiseKit
 */
+
 extension NSNotificationCenter {
-    public class func once(name: String) -> NotificationPromise {
+    public class func once(name: String) -> NotificationPromise<[NSObject: AnyObject]> {
         return NSNotificationCenter.defaultCenter().once(name)
     }
-
-    public func once(name: String) -> NotificationPromise {
-        let (promise, fulfill, _) = NotificationPromise.go()
+    
+    public func once(name: String) -> NotificationPromise<[NSObject: AnyObject]> {
+        let (promise, fulfill, _) = NotificationPromise<[NSObject: AnyObject]>.go()
         let id = addObserverForName(name, object: nil, queue: nil, usingBlock: fulfill)
         promise.then(on: zalgo) { _ in self.removeObserver(id) }
         return promise
     }
 }
 
-public class NotificationPromise: Promise<[NSObject: AnyObject]> {
+public class NotificationPromise<T>: Promise<T> {
     private let (parentPromise, parentFulfill, parentReject) = Promise<NSNotification>.pendingPromise()
-
+    
     public func asNotification() -> Promise<NSNotification> {
         return parentPromise
     }
-
-    private class func go() -> (NotificationPromise, (NSNotification) -> Void, (ErrorType) -> Void) {
+    
+    private class func go() -> (NotificationPromise<[NSObject: AnyObject]>, (NSNotification) -> Void, (ErrorType) -> Void) {
         var fulfill: (([NSObject: AnyObject]) -> Void)!
         var reject: ((ErrorType) -> Void)!
-        let promise = NotificationPromise { f, r in
-            fulfill = f
-            reject = r
-        }
+                let promise = NotificationPromise<[NSObject: AnyObject]> { f, r in
+                    fulfill = f
+                    reject = r
+                }
         promise.parentPromise.then { fulfill($0.userInfo ?? [:]) }
         promise.parentPromise.error { reject($0) }
         return (promise, promise.parentFulfill, promise.parentReject)
     }
-
-    private override init(@noescape resolvers: (fulfill: ([NSObject: AnyObject]) -> Void, reject: (ErrorType) -> Void) throws -> Void) {
+    
+    private override init(@noescape resolvers: (fulfill: (T) -> Void, reject: (ErrorType) -> Void) throws -> Void) {
         super.init(resolvers: resolvers)
     }
 }
@@ -60,8 +61,8 @@ extension NSNotificationCenter {
         return NSNotificationCenter.defaultCenter().once(name, timeout:timeout)
     }
     
-    public func once(name: String, timeout: NSTimeInterval) -> Promise<[NSObject: AnyObject]> {
-        let (promise, fulfill, reject) = NotificationPromise.go()
+    private func once(name: String, timeout: NSTimeInterval) -> Promise<[NSObject: AnyObject]> {
+        let (promise, fulfill, reject) = NotificationPromise<[NSObject:AnyObject]>.go()
         let id = addObserverForName(name, object: nil, queue: nil, usingBlock: fulfill)
         after(timeout).then { reject(NSError(code: VVErrorCode.Network.rawValue)) }
         promise.always(on: zalgo) { _ in self.removeObserver(id) }
